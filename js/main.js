e3205ee4aa27c3e09b391d5caa94bfbca85779f5
sergei alekseev <52599099+frozendrakkon@users.noreply.github.com>
2020-12-20 13:15:29 +0300

    add header

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$('.top-slider').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: `<button type="button" class="slick-arrow slick-next"><img src="./images/next.svg" alt="next"></button>`,
+    prevArrow: `<button type="button" class="slick-arrow slick-prev"><img src="./images/prew.svg" alt="prew"></button>`,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+            arrows: false
+        }
+      }
+    ]
+  });
